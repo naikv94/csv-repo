e74effe09fa490be15afdea77f58feb72ef779ae
naikv94 <naikvishal94@gmail.com>
2022-02-16 19:55:01 +0530

    form update

--- api/forms.py
+++ api/forms.py
@@ -9,7 +9,6 @@ class FileForm(forms.ModelForm):
         Widget = {'csv_file':forms.FileInput(attrs={'class': 'form-control'})}
     
     def clean(self):
-        print('clean')
         file_path = "media/" + self.cleaned_data.get('csv_file').name
         if os.path.isfile(file_path):
             raise forms.ValidationError('File already exists! Rename and upload again') 
