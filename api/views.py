61bd3cb9144243c67c558935ccc7d15f1eb0afff
naikv94 <naikvishal94@gmail.com>
2022-02-17 11:39:29 +0530

    update functionality for countingStrings

--- api/views.py
+++ api/views.py
@@ -51,7 +51,7 @@ def getData(request):
         if from_date and to_date:
             data = Data.objects.filter(timestamp__range=[from_date, to_date])
             if data:
-                str_data = CountStrings(data)
+                str_data = calculateData(data)
                 ToCSV(str_data)
                 messages.success(request, 'Data fetched and record generated!!')
                 return render(request,'index.html',{'data':data,'form':form})
@@ -64,30 +64,6 @@ def getData(request):
         data = None
         return render(request,'index.html',{'data':data,'form':form})
     return render(request,'index.html',{})
-
-
-# Function to count number detected_objects in db
-def CountStrings(data):
-    data_count = []
-    list_data = []
-    data_values = list(data.values())
-    for i in range(len(data)):
-        temp_data = data_values[i]['objects_detected'].split(',')
-        list_data.append(temp_data)
-    str_data = ['jewellery','watch','gun','mobile','knife','e-cigarette','scissor','nail_cutter','cigar_cutter','laptop','lighter',
-    'battrey','ammo','gold','plier','cigarette_case','cigarette','cigar']
-    for i in range(len(str_data)):
-        count = 0
-        for j in range(len(list_data)):
-            for k in range(len(list_data[j])):
-                if str_data[i] == list_data[j][k]:
-                    count = count + 1
-        data_count.append(count)
-    context = {
-        'str_data' : str_data,
-        'data_count' : data_count
-    }
-    return context
 
 
 # Function to deploy calculated specific data to .csv file 
@@ -110,3 +86,42 @@ def ToCSV(context):
                 writer.writerow(data)
     except IOError:
         print("I/O error")
+
+# Calculate number of strings in generated queryset
+def calculateData(data):
+    data_count = []
+    list_data = []
+    data_values = list(data.values())
+    for i in range(len(data)):
+        temp_data = data_values[i]['objects_detected'].split(',')
+        list_data.append(temp_data)
+    str_data = calculateStrings()
+    for j in range(len(str_data)): # Calculate number of string from generated data as per dates
+        count = 0
+        for i in range(len(list_data)):
+            for k in range(len(list_data[i])):
+                if str_data[j] == list_data[i][k]:
+                    count = count + 1
+        data_count.append(count)
+    context = {
+        'str_data' : str_data, # data in list format
+        'data_count' : data_count  # data in list format
+    }
+    return context
+
+
+# Calculate number of strings in all querysets
+def calculateStrings():
+    data_query = Data.objects.all()
+    data = list(data_query.values())
+    str_data = []
+    objects_list = []
+    for i in range(len(data)): # gettting all object_detected string in list format
+        temp = data[i]['objects_detected'].split(',')
+        objects_list.append(temp)
+    for j in range(len(objects_list)): # Putting single string into a new list
+        for k in range(len(objects_list[j])):
+            temp2 = objects_list[j][k]
+            str_data.append(temp2)
+    unique_strings_set = set(str_data) # Only unique strings are obtained
+    return list(unique_strings_set)  # Converting into list for further operations
